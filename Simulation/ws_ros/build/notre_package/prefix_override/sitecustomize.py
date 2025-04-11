00b63c2b3def419f1dd5824d2ea60d535ab93431
maxlef <165215264+maxxlef@users.noreply.github.com>
2025-04-11 16:57:37 +0200

    Delete Simulation/ws_ros/build directory

--- Simulation/ws_ros/build/notre_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/maxime/Documents/ENSTA/UE4.1_ROS/voiture2A-ros-main/Simulation/ws_ros/install/notre_package'
--- Simulation/ws_ros/build/notre_package/share/notre_package/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/maxime/Documents/ENSTA/UE4.2_Simulation/modrob2025/ROS/ws_ros/build/notre_package"
